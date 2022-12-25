39f71a10f0811a3f34bbcb06f3a7d4526c3488f8
Renan Goulart <64268235+RenanGoulart@users.noreply.github.com>
2022-12-25 13:45:56 -0300

    fix: configs and app name

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@global': './src/global',
+            '@hooks': './src/hooks',
+            '@screens': './src/screens',
+            '@routes': './src/routes',
+            '@services': './src/services',
+            '@utils': './src/utils',
+            '@validation': './src/validation',
+          },
+        },
+      ],
+    ],
   };
 };
